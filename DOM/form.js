341ebcf3cc13c88afb406bd7284b8c40c48a90ea
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2025-09-14 12:46:08 +0530

    form events

---
+++ DOM/form.js
@@ -0,0 +1,15 @@
+let form = document.querySelector("form")
+
+form.addEventListener("submit",function(event){
+    event.preventDefault(); // stop navigation "/action" after event submitt occur
+    // console.log(event);
+    let userName = document.querySelector("#username");
+    let userPassword = document.querySelector("#password")
+    // console.dir(input.value);
+
+    console.log(`User Name is : ${userName.value}`);
+    console.log(`User password is : ${userPassword.value}`);
+    
+    
+    
+})
