276f857671157b4d6dba3b55aaa10d25c2f20c32
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2025-09-16 23:04:52 +0530

    basic project5 v2

--- Project/project5.js
+++ Project/project5.js
@@ -1,15 +1,27 @@
-let btn1 = document.querySelector(".btn1")
-let h1 = document.querySelector("h1")
-let btn2 = document.querySelector(".btn2")
-
 let body = document.querySelector("body")
+let h1 = document.querySelector("h1")
+let toggle = document.querySelector("button")
 
-btn1.addEventListener("click",()=>{    
-    body.style.backgroundColor = "black"
-    h1.style.color = "white"
+toggle.addEventListener("click",function(){
+    darkLight()
 })
+function darkLight(){
+    document.body.classList.toggle("dark")
+    h1.classList.toggle("light")
+}
 
-btn2.addEventListener("click",()=>{    
-    body.style.backgroundColor = "white"
-    h1.style.color = "black"
-})
+
+// basic Approach -> create 2 button
+
+// let btn1 = document.querySelector(".btn1")
+// let btn2 = document.querySelector(".btn2")
+
+// btn1.addEventListener("click",()=>{    
+//     body.style.backgroundColor = "black"
+//     h1.style.color = "white"
+// })
+
+// btn2.addEventListener("click",()=>{    
+//     body.style.backgroundColor = "white"
+//     h1.style.color = "black"
+// })
