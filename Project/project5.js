7163848a6fbd629a179adaa0808be2372ddb6aaf
Abhishek Tiwari <abhishektiwari1258@gmail.com>
2025-09-17 00:27:30 +0530

    basic project 3 toggling using single btn

--- Project/project5.js
+++ Project/project5.js
@@ -1,14 +1,40 @@
-let body = document.querySelector("body")
+// 3rd Approach using toggle
+
+// let body = document.querySelector("body")
 let h1 = document.querySelector("h1")
 let toggle = document.querySelector("button")
 
-toggle.addEventListener("click",function(){
-    darkLight()
-})
-function darkLight(){
-    document.body.classList.toggle("dark")
-    h1.classList.toggle("light")
+function changeBackgroundColor(color){
+    document.body.style.backgroundColor = color;
 }
+
+function textColor(color){
+    h1.style.color = color;
+}
+
+toggle.addEventListener("click",function(){
+    console.log(document.body.style.backgroundColor);
+    let currentColor = document.body.style.backgroundColor
+    if(currentColor == "" || currentColor == "white"){
+        changeBackgroundColor("black")
+        toggle.innerText = "dark Mode"
+        textColor("white")
+    }else{
+        changeBackgroundColor("white")
+        toggle.innerText = "light Mode"
+        textColor("black")
+    }  
+})
+
+
+// 2nd Approach using toggle
+// toggle.addEventListener("click",function(){
+//     darkLight()
+// })
+// function darkLight(){
+//     document.body.classList.toggle("dark")
+//     h1.classList.toggle("light")
+// }
 
 
 // basic Approach -> create 2 button
